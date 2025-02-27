29fe5dfa1dab59ed049540dcfff5d81533513225
Likitha40 <paralikithalekhya@gmail.com>
2025-02-27 10:30:25 +0530

    Add files via upload

---
+++ week 1/Studentsdetails.java
@@ -0,0 +1,8 @@
+public class Studentsdetails {
+  public static void main(String[] args){
+    System.out.println("Name:Likitha Lekhya");
+    System.out.println("rollno:24240");
+    System.out.println("course:btech");
+  }
+}
+  
