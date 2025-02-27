2f95053387f36a824c49d5234b513dc5d42487d6
Likitha40 <paralikithalekhya@gmail.com>
2025-02-27 10:21:19 +0530

    Delete javafiles/Studentsdetails.java

--- javafiles/Studentsdetails.java
+++
@@ -1,8 +0,0 @@
-public class Studentsdetails {
-  public static void main(String[] args){
-    System.out.println("Name:Likitha Lekhya");
-    System.out.println("rollno:24240");
-    System.out.println("course:btech");
-  }
-}
-  
