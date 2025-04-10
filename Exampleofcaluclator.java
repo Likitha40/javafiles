a2d80247da65bcf041bb7a23ceb2b61ef7ce9941
Likitha40 <paralikithalekhya@gmail.com>
2025-04-10 16:47:39 +0530

    Delete Exampleofcaluclator.java

--- Exampleofcaluclator.java
+++
@@ -1,8 +0,0 @@
-public class Exampleofcaluclator {
-    public static void main(String[] args) {
-        CalculatorOverloading obj = new CalculatorOverloading();
-        System.out.println("Addition of two integers 2 and 3: " + obj.add(2, 3));
-        System.out.println("Addition of two doubles 2.5 and 3.5: " + obj.add(2.5, 3.5));
-        System.out.println("Addition of three integers 2, 3, and 4: " + obj.add(2, 3, 4));
-    }
-}
